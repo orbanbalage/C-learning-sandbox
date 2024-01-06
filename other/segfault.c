e8edfc9985c4c0b1e2310648a7bc36046fcb2e81
Balázs Orbán <285574+orbanbalage@users.noreply.github.com>
2024-01-06 16:14:28 +0100

    corrected C null pointer segfault

--- other/segfault.c
+++ other/segfault.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-     char *p;
+	char *p = NULL; //nullptr = C++11
 	//int *a;
 	int *ptr = 0x0;
-
-     *p = "abc123";
+	
+	*p = "abc123"; //segfault
 	//*a = 12;	//bus error
 	printf("%f\n", *ptr);	//segfault
-     return 0 ;
+	return 0 ;
 }
